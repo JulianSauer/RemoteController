55b499ad79c8192035bdfb870b68763c5828daeb
Julian Sauer <julian_sauer@gmx.net>
2019-06-28 19:32:28 +0200

    Temp fix repeated calls

--- remote/remote.go
+++ remote/remote.go
@@ -5,30 +5,15 @@ import (
     "strconv"
     "github.com/Tinkerforge/go-api-bindings/remote_switch_v2_bricklet"
     "gopkg.in/resty.v1"
-    "time"
     "net/url"
     "os/exec"
 )
 
-const INPUT_DELY = 1000
-
-var lastButtonPress = currentTime()
-var lastHouseCode uint8 = 255
-var lastReceiverCode uint8 = 255
-var lastSwitchTo remote_switch_v2_bricklet.SwitchTo = 255
-
 var remote_config *Remote_Config
 
 func ParseCall(houseCode uint8, receiverCode uint8, switchTo remote_switch_v2_bricklet.SwitchTo, repeats uint16) {
-    if currentTime()-lastButtonPress < INPUT_DELY &&
-        lastHouseCode == houseCode &&
-        lastReceiverCode == receiverCode &&
-        lastSwitchTo == switchTo {
+    if repeats != 1 {
         return
-    } else {
-        lastHouseCode = houseCode
-        lastReceiverCode = receiverCode
-        lastSwitchTo = switchTo
     }
 
     if remote_config == nil {
@@ -36,7 +21,6 @@ func ParseCall(houseCode uint8, receiverCode uint8, switchTo remote_switch_v2_br
         remote_config, e = Load()
         if e != nil {
             fmt.Println(e.Error())
-            lastButtonPress = currentTime()
             return
         }
     }
@@ -66,11 +50,6 @@ func ParseCall(houseCode uint8, receiverCode uint8, switchTo remote_switch_v2_br
         }
     }
 
-    lastButtonPress = currentTime()
-}
-
-func currentTime() int64 {
-    return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
 func isValidUrl(input string) bool {
